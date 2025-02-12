7ce65f16f08ceff56555a25081fef18b647f170a
Jonathan Klepsch <Mage882@GMail.com>
2025-02-12 12:20:05 -0700

    Completed snake game

--- day20_21/snake_game/snake.py
+++ day20_21/snake_game/snake.py
@@ -17,12 +17,7 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle()
-            new_segment.shape("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1): # Key point: start= 2, stop= 0, step= -1
@@ -44,3 +39,14 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_segment = Turtle()
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
