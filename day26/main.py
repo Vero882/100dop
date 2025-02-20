491e9158a1b2886f98ee12ed1c19171eeffa0d9c
Jonathan Klepsch <Mage882@GMail.com>
2025-02-20 14:34:56 -0700

    Updated project to loop

--- day26/main.py
+++ day26/main.py
@@ -8,7 +8,18 @@ nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 user_input = input("Enter a word: ").upper()
 
+running = True
 
-nato_list = [nato_dict[letter] for letter in user_input]
-print(nato_list)
+while running:
+    nato_list = [nato_dict[letter] for letter in user_input]
+    print(nato_list)
 
+    running_check = input("Would you like to enter another word? (y/n): ").lower()
+    if running_check == "n":
+        running = False
+        print("Goodbye")
+    elif running_check == "y":
+        user_input = input("Enter a word: ").upper()
+    else:
+        print("Invalid input. Goodbye")
+        running = False
